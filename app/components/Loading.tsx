d1ac60602d1a1484192106319cdf1922330f8a1d
Joe bae <lineanity@gmail.com>
2024-12-04 13:07:16 +0700

    testing farcaster frame

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -3,7 +3,7 @@ export default function Loading() {
         <div className="flex items-center justify-center h-screen">
             <div className="flex items-center space-x-2">
                 <div className="w-4 h-4 rounded-full bg-blue-500 animate-bounce"></div>
-                <div className="w-4 h-4 rounded-full bg-green-500 animate-bounce animation-delay-200"></div>
+                <div className="w-4 h-4 rounded-full bg-yellow-600 animate-bounce animation-delay-200"></div>
                 <div className="w-4 h-4 rounded-full bg-purple-800 animate-bounce animation-delay-400"></div>
             </div>
         </div>
